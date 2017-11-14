bcf7f41742e38ca899c7b19f19e42f567691505c
Rob <robe16@hotmail.co.uk>
2017-11-14 23:05:03 +0000

    'last ip' now stored in ip.txt file (bug fix - file being opened incorrect).

--- src/ip/ip_previous.py
+++ src/ip/ip_previous.py
@@ -1,7 +1,7 @@
 import os
 
 def get_last_ip():
-    file = open(os.path.join(os.path.dirname(__file__), 'ip.txt'), 'r')
+    file = open(os.path.join(os.path.dirname(__file__), 'last_ip.txt'), 'r')
     ip = file.read()
     file.close()
     #
@@ -9,6 +9,6 @@ def get_last_ip():
 
 
 def set_last_ip(ip):
-    file = open(os.path.join(os.path.dirname(__file__), 'ip.txt'), 'w')
+    file = open(os.path.join(os.path.dirname(__file__), 'last_ip.txt'), 'w')
     file.write(ip)
     file.close()
